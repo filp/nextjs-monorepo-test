aec5f00dd263d21214ce66e67828273d0f95cf9d
Filipe Dobreira <filipe.dobreira@aula.education>
2021-11-22 01:40:31 +0000

    add subtle first load fade in

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -6,6 +6,16 @@ import { Quote } from '../components/Quote';
 import { Button } from '../components/Button';
 
 import type { Quote as QuoteType } from 'quotes';
+import { Transition } from '@headlessui/react';
+
+const Spinner = ({ text }: { text: string }) => (
+  <div className="text-blue-500 animate-pulse">
+    <span className="animate-spin inline-block mr-2">
+      <AiOutlineLoading3Quarters />
+    </span>
+    {text}
+  </div>
+);
 
 const Home = () => {
   const [quote, setQuote] = useState<QuoteType>();
@@ -18,22 +28,23 @@ const Home = () => {
     getSingleRandomQuote();
   }, []);
 
-  return quote ? (
+  return (
     <>
-      <Quote quote={quote!} />
-      <div className="p-4 text-center">
-        <Button onClick={() => getSingleRandomQuote()}>
-          <AiOutlineReload /> Let{"'"}s see another one
-        </Button>
-      </div>
+      {!quote && <Spinner text="Loading a great quote..." />}
+      <Transition
+        show={!!quote}
+        enter="transition-opacity ease-in"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+      >
+        {quote && <Quote quote={quote} />}
+        <div className="p-4 text-center">
+          <Button onClick={() => getSingleRandomQuote()}>
+            <AiOutlineReload /> Let{"'"}s see another one
+          </Button>
+        </div>
+      </Transition>
     </>
-  ) : (
-    <div className="text-blue-500 animate-pulse">
-      <span className="animate-spin inline-block mr-2">
-        <AiOutlineLoading3Quarters />
-      </span>
-      Loading a great quote..
-    </div>
   );
 };
 
